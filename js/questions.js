18aa4d9a154e8d1424665ab8d4acd32b240294cc
Aryan Antal <you@example.com>
2021-09-21 22:05:32 +0530

    Online Quiz Application

---
+++ js/questions.js
@@ -0,0 +1,135 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [{
+        numb: 1,
+        question: "What does HTML stand for?",
+        answer: "Hyper Text Markup Language",
+        options: [
+            "Hyper Text Preprocessor",
+            "Hyper Text Markup Language",
+            "Hyper Text Multiple Language",
+            "Hyper Tool Multi Language"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What does CSS stand for?",
+        answer: "Cascading Style Sheet",
+        options: [
+            "Common Style Sheet",
+            "Colorful Style Sheet",
+            "Computer Style Sheet",
+            "Cascading Style Sheet"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What does PHP stand for?",
+        answer: "Hypertext Preprocessor",
+        options: [
+            "Hypertext Preprocessor",
+            "Hypertext Programming",
+            "Hypertext Preprogramming",
+            "Hometext Preprocessor"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What does SQL stand for?",
+        answer: "Structured Query Language",
+        options: [
+            "Stylish Question Language",
+            "Stylesheet Query Language",
+            "Statement Question Language",
+            "Structured Query Language"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What does XML stand for?",
+        answer: "eXtensible Markup Language",
+        options: [
+            "eXtensible Markup Language",
+            "eXecutable Multiple Language",
+            "eXTra Multi-Program Language",
+            "eXamine Multiple Language"
+        ]
+    },
+
+    {
+        numb: 6,
+        question: "HTML uses",
+        answer: "Fixed tags defined by the language",
+        options: [
+            "Pre-specified tags",
+            "Tags only for linking",
+            "Fixed tags defined by the language",
+            "User defined tags",
+        ]
+    },
+
+    {
+        numb: 7,
+        question: "The year in which HTML was first proposed _______.",
+        answer: "1990",
+        options: [
+            "1980",
+            "1990",
+            "1979",
+            "1985"
+        ]
+    },
+
+    {
+        numb: 8,
+        question: "Fundamental HTML Block is known as ___________.",
+        answer: "HTML Tag",
+        options: [
+            "HTML Body",
+            "HTML Tag",
+            "HTML Attribute",
+            "HTML Element"
+        ]
+    },
+
+
+    {
+        numb: 9,
+        question: "Which type of JavaScript language is ___",
+        answer: "Object-Based",
+        options: [
+            "Object-Oriented",
+            "Assembly-language",
+            "Object-Based",
+            "High-level"
+        ]
+    },
+
+    {
+        numb: 10,
+        question: " The property in CSS used to change the background color of an element is -",
+        answer: "background-color",
+        options: [
+            "bgcolor",
+            "background-color",
+            "color",
+            "All of the above"
+        ]
+    },
+
+
+
+    // you can uncomment the below codes and make duplicate as more as you want to add question
+    // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+    //   {
+    //   numb: 6,
+    //   question: "Your Question is Here",
+    //   answer: "Correct answer of the question is here",
+    //   options: [
+    //     "Option 1",
+    //     "option 2",
+    //     "option 3",
+    //     "option 4"
+    //   ]
+    // },
+];
